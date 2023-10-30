2bd7a90192c0a7538495a56acdf70d5941ad762f
Liath Vliegers <livliege@f0r2s3.codam.nl>
2023-10-30 18:32:36 +0100

    LIBFT DONE WOOOOOOOOOOOOOOOOOOOOOOOOH

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -1,38 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: livliege <livliege@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/28 16:22:26 by livliege          #+#    #+#             */
-/*   Updated: 2023/10/29 17:31:55 by livliege         ###   ########.fr       */
+/*   Created: 2023/10/30 18:28:41 by livliege          #+#    #+#             */
+/*   Updated: 2023/10/30 18:28:45 by livliege         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-Function name 		ft_lstadd_front
-Prototype 			void ft_lstadd_front(t_list **lst, t_list *new);
+Function name 		ft_lstadd_back
+Prototype 			void ft_lstadd_back(t_list **lst, t_list *new);
 Turn in files 		-
-Parameters 			lst:	The address of a pointer to the first link of a list.
-					new:	The address of a pointer to the node to be 
-							added to the list.
+Parameters 			lst: The address of a pointer to the first link of a list.
+					new: The address of a pointer to the node to be 
+					added to the list.
 Return value 		None
 External functs. 	None
-Description 		Adds the node ’new’ at the beginning of the list.
+Description 		Adds the node ’new’ at the end of the list.
 */
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*new_node;
+	t_list	*last;
 
 	if (lst == NULL || new == NULL)
 		return ;
 	new_node = new;
-	if (new_node == NULL)
+	if (*lst == NULL)
+	{
+		*lst = new_node;
 		return ;
-	new_node->next = *lst;
-	*lst = new_node;
+	}
+	else
+	{
+		last = *lst;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new_node;
+		return ;
+	}
 }
--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -1,48 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: livliege <livliege@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/28 16:22:23 by livliege          #+#    #+#             */
-/*   Updated: 2023/10/29 17:31:54 by livliege         ###   ########.fr       */
+/*   Created: 2023/10/30 18:28:52 by livliege          #+#    #+#             */
+/*   Updated: 2023/10/30 18:28:54 by livliege         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-Function name 		ft_lstadd_back
-Prototype 			void ft_lstadd_back(t_list **lst, t_list *new);
+Function name 		ft_lstadd_front
+Prototype 			void ft_lstadd_front(t_list **lst, t_list *new);
 Turn in files 		-
-Parameters 			lst: The address of a pointer to the first link of a list.
-					new: The address of a pointer to the node to be 
-					added to the list.
+Parameters 			lst:	The address of a pointer to the first link of a list.
+					new:	The address of a pointer to the node to be 
+							added to the list.
 Return value 		None
 External functs. 	None
-Description 		Adds the node ’new’ at the end of the list.
+Description 		Adds the node ’new’ at the beginning of the list.
 */
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	t_list	*new_node;
-	t_list	*last;
 
 	if (lst == NULL || new == NULL)
 		return ;
 	new_node = new;
-	if (*lst == NULL)
-	{
-		*lst = new_node;
+	if (new_node == NULL)
 		return ;
-	}
-	else
-	{
-		last = *lst;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new_node;
-		return ;
-	}
+	new_node->next = *lst;
+	*lst = new_node;
 }
